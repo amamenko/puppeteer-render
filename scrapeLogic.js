d2505e204da0a7317b1629af5838fd4e3bc73c7f
Avi Mamenko <avimamenko@gmail.com>
2023-02-20 14:29:00 -0500

    Updated args and executable path for Puppeteer launch

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -1,7 +1,19 @@
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 const scrapeLogic = async (res) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   try {
     const page = await browser.newPage();
 
